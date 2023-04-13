b3c8813fff84eb28adb68077547e8545aec4bdd6
tadahiroueta <tadahiroueta@gmail.com>
2023-04-13 15:48:55 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ export default function App() {
     <div className="App">
 
       <header>
-        <h2>Programming Portfolio&emsp;&emsp;&emsp;Dec 2021 - Present</h2>
+        <h2><span>Programming Portfolio</span>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Dec 2021 - Present</h2>
         <h4>tadahiroueta.com</h4>
       </header>
 
       <ul>
         <li>
           <a href='https://digit.tadahiroueta.com'>
-            <h3>Digit-Recognition Neural Network</h3>
+            <h4>Digit-Recognition Neural Network</h4>
           </a>
         </li>
         <li>
           <a href='https://pheromones.tadahiroueta.com'>
-            <h3>Ant Colony Heuristic - Salesperson Problem</h3>
+            <h4>Ant Colony Heuristic - Salesperson Problem</h4>
           </a>
         </li>
         <li>
           <a href='https://playlist.tadahiroueta.com'>
-            <h3>Spotify Client Search Algorithm</h3>
+            <h4>Spotify Client Search Algorithm</h4>
           </a>
         </li>
         <li>
           <a href='https://mmvacuo.tadahiroueta.com/'>
-            <h3>Commercial Website for scientific equipment supplier</h3>
+            <h4>Commercial Website for scientific equipment supplier</h4>
           </a>
         </li>
       </ul>
