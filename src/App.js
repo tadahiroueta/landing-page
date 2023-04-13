30b4ea0257491880b5123d90e608c70ca6184fe7
tadahiroueta <tadahiroueta@gmail.com>
2023-04-13 15:20:54 -0500

    Usable

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
 
       <header>
-        {/* tabs in between */}
-        <h2>Programming Portfolio          Dec 2021 - Present</h2>
+        <h2>Programming Portfolio&emsp;&emsp;&emsp;Dec 2021 - Present</h2>
         <h4>tadahiroueta.com</h4>
       </header>
 
-      <div className="Sites">
-        <a href='https://digit.tadahiroueta.com'>
-          <h3>Digit-Recognition Neural Network</h3>
-        </a>
-        <a href='https://pheromones.tadahiroueta.com'>
-          <h3>Ant Colony Heuristic - Salesperson Problem</h3>
-        </a>
-        <a href='https://playlist.tadahiroueta.com'>
-          <h3>Spotify Client Search Algorithm</h3>
-        </a>
-        <a href='https://mmvacuo.tadahiroueta.com/'>
-          <h3>Commercial Website for scientific equipment supplier</h3>
-        </a>
-      </div>
+      <ul>
+        <li>
+          <a href='https://digit.tadahiroueta.com'>
+            <h3>Digit-Recognition Neural Network</h3>
+          </a>
+        </li>
+        <li>
+          <a href='https://pheromones.tadahiroueta.com'>
+            <h3>Ant Colony Heuristic - Salesperson Problem</h3>
+          </a>
+        </li>
+        <li>
+          <a href='https://playlist.tadahiroueta.com'>
+            <h3>Spotify Client Search Algorithm</h3>
+          </a>
+        </li>
+        <li>
+          <a href='https://mmvacuo.tadahiroueta.com/'>
+            <h3>Commercial Website for scientific equipment supplier</h3>
+          </a>
+        </li>
+      </ul>
 
     </div>
   );
 }
-
-export default App;
